9af14b2a39182e2d64d6423d3c917759eedf99a8
ZizyLiz <muh.aziztau05@gmail.com>
2024-06-04 20:24:30 +0700

    just some modification on layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -42,7 +42,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="{{url('dashboard')}}">
+                <a class="nav-link" href="{{url('/')}}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
